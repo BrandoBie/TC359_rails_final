1e78b7b227be5cfdfe0b4c78b9c55abbf7491a9d
Brandon Bielicki <bielick3@msu.edu>
2015-03-22 16:42:35 -0400

    Init

---
+++ app/helpers/static_helper.rb
@@ -0,0 +1,7 @@
+module StaticHelper
+
+def dateTime
+    Time.now.strftime('%B %-d, %Y at %-l:%M%P')
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyNewApp::Application.config.secret_key_base = 'd7334332a61df4b41f12daac9dd2d6e4e26c670252e5076e3ebeb975918e7d7943195920c824c3980f8d15efac19efc760a443714905e58986fe1a942064550f'
